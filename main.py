22aac02383e02bc5fadd6f46c93e950c78a5518d
eficek <eficek@protonmail.com>
2021-04-01 02:35:23 +0000

    feat: bot now sends connect links instead of console info

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import discord
 import os
-import time
 from keep_alive import keep_alive
 
 client = discord.Client()
@@ -30,6 +29,8 @@ async def on_message(message):
         if message.content.startswith('!'):
             if message.content.startswith('!helloworld'):
                 await message.channel.send('hello world')
+            elif message.content.startswith('!help'):
+                await message.channel.send('see pinned :)')
             elif message.content.startswith('!connect'):
                 await connect(message, message.content[9:])
             elif message.content.startswith('!endpug'):
@@ -40,21 +41,30 @@ async def on_message(message):
 
 
 async def connect(message, info):
-    for user in RED_VC.members:
-        try:
-            await user.send(info, delete_after=600)
-        except Exception as e:
-            print(e)
-            await message.channel.send(
-                f'{user.name} appears to have dms off, connect not sent')
-    for user in BLU_VC.members:
-        try:
-            await user.send(info, delete_after = 600)
-        except Exception as e:
-            print(e)
-            await message.channel.send(
-                f'{user.name} appears to have dms off, connect not sent')
-    await message.channel.send(f'connect sent: {message.content[9:]}')
+    list = message.content.split()
+    if len(list) == 3:
+        for user in RED_VC.members:
+            try:
+                await user.send(f'steam://connect/{list[1]}/{list[2]}',
+                                delete_after=600)
+            except Exception as e:
+                print(e)
+                await message.channel.send(
+                    f'{user.name} appears to have dms off, connect not sent')
+        for user in BLU_VC.members:
+            try:
+                await user.send(f'steam://connect/{list[1]}/{list[2]}',
+                                delete_after=600)
+            except Exception as e:
+                print(e)
+                await message.channel.send(
+                    f'{user.name} appears to have dms off, connect not sent')
+        await message.channel.send(
+            f'connect sent: steam://connect/{list[1]}/{list[2]}')
+    else:
+        await message.channel.send(
+            'it appears the submitted info is not formatted correctly, be sure to submit in the form of "!connect [ip:port] [password]"'
+        )
 
 
 async def teams_to_picking(message):
@@ -63,15 +73,13 @@ async def teams_to_picking(message):
             await user.move_to(PICKING_VC)
         except Exception as e:
             print(e)
-            await message.channel.send(
-                f'failed not move {user.name}')
+            await message.channel.send(f'failed not move {user.name}')
     for user in BLU_VC.members:
         try:
             await user.move_to(PICKING_VC)
         except Exception as e:
             print(e)
-            await message.channel.send(
-                f'failed not move {user.name}')
+            await message.channel.send(f'failed not move {user.name}')
     await message.channel.send('teams moved to picking')
 
 
@@ -87,7 +95,8 @@ async def post_fatkids(message):
                 'there appear to be no fatkids in channel')
     except Exception as e:
         print(e)
-        await message.channel.send(f'{DLPHN.mention} exception occured, logged to console.')
+        await message.channel.send(
+            f'{DLPHN.mention} exception occured, logged to console.')
 
 
 keep_alive()
