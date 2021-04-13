e667e30d8dcee77cb47da0a30986b897b3ff074b
TheRebelCreeper <therebelcreeper@gmail.com>
2021-04-13 02:48:08 -0700

    Cleaned some shit up

--- keep_alive.py
+++ keep_alive.py
@@ -1,17 +1,15 @@
-from flask import Flask
 from threading import Thread
 
-app = Flask('')
+from flask import Flask
 
+app = Flask('')
 
 @app.route('/')
 def home():
     return "hello world, i am abusing a free service"
 
-
 def run():
     app.run(host='0.0.0.0', port=8080)
-
 
 def keep_alive():
     t = Thread(target=run)
